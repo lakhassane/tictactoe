9442140f77dff5c8866ff51a77161033475ee4c0
mamadou <mamadou.lakhassane@gmail.com>
2021-03-21 19:03:18 +0000

    comment port

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var app = express();
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
 
-/* const hostname = "127.0.0.1";
-const port = 3000; */
+// const hostname = "127.0.0.1";
+// const port = 3000;
 const PORT = process.env.PORT || 5000;
 
 const opponent = "o";
